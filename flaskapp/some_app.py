4ee5db62fc18e72a26f902d0cb64766d5bd6f191
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2021-06-08 23:16:09 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import StringField, SubmitField, TextField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from flask_bootstrap import Bootstrap
@@ -85,7 +85,7 @@ class IzForm(FlaskForm):
         FileRequired(),
         FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
     recaptcha = RecaptchaField()
-    user = BooleanField()
+    user = TextField()
     submit = SubmitField('send')
  
  
